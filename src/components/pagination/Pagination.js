673653000c8d5036a17f4f7fea4aa05b2bf5e382
skorobahatko <maxim.skorobagatko@gmail.com>
2020-09-07 22:23:00 +0300

    debugged pagination

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -15,45 +15,57 @@ const Pagination = (props) => {
             {
                 (value) => {
                     const {isDarkTheme} = value;
-                    console.log (isDarkTheme);
                     return (
                         <div className={`container-for-pagination ${isDarkTheme ? 'dark' : null}`}>
-                            {!props.isLoading ? <div className='container-for-buttons-of-pagination'>
-                                {thisPage > 2 ? <Link to={`${1}`}
-                                                      className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
-                                                      style={{ textDecoration: 'none' }}
-                                >
-                                    1
-                                </Link> : null}
-                                {thisPage > 1 ? <Link to={`${thisPage - 1}`}
-                                                      className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
-                                                      style={{ textDecoration: 'none' }}
-                                >
-                                    {thisPage - 1}
-                                </Link> : null}
-                                <Link to={`${thisPage}`}
-                                      className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
-                                      style={{ textDecoration: 'none' }}
-                                >
-                                    {thisPage}
-                                </Link>
-                                <Link to={`${thisPage + 1}`}
-                                      className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
-                                      style={{ textDecoration: 'none' }}
-                                >
-                                    {thisPage + 1}
-                                </Link>
-                                <Link to={`${totalPages}`}
-                                      className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
-                                      style={{ textDecoration: 'none' }}
-                                >
-                                    {totalPages}
-                                </Link>
-                            </div> : null}
+                            {!props.isLoading ?
+                                <div className='container-for-buttons-of-pagination'>
+                                    {
+                                        thisPage > 2 ?
+                                            <Link to={`${1}`}
+                                                  className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
+                                                  style={{ textDecoration: 'none' }}>
+                                                1
+                                            </Link> : null
+                                    }
+                                    {
+                                        thisPage > 1 ?
+                                            <Link to={`${thisPage - 1}`}
+                                                  className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
+                                                  style={{ textDecoration: 'none' }}
+                                            >
+                                                {thisPage - 1}
+                                            </Link> : null
+                                    }
+                                    <Link to={`${thisPage}`}
+                                          className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
+                                          style={{ textDecoration: 'none' }}
+                                    >
+                                        {thisPage}
+                                    </Link>
+                                    {
+                                        thisPage + 1 < totalPages ?
+                                            <Link to={`${thisPage + 1}`}
+                                                  className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
+                                                  style={{ textDecoration: 'none' }}
+                                            >
+                                                {thisPage + 1}
+                                            </Link> : null
+                                    }
+                                    {
+                                        thisPage + 2 < totalPages ?
+                                            <Link to={`${totalPages}`}
+                                                  className={`btn-pagination ${isDarkTheme ? 'dark' : null}`}
+                                                  style={{ textDecoration: 'none' }}
+                                            >
+                                                {totalPages}
+                                            </Link> : null
+                                    }
+                                </div> : null}
                         </div>)
                 }
             }
         </DarkThemeContext.Consumer>
+
     )
 }
 export default withRouter(Pagination);
